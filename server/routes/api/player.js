9e16a6f2ce615c64b5dac292c4708a4a6342e62a
anaumenk <al13ra@gmail.com>
2020-01-20 19:17:09 +0200

    return deleted file

---
+++ server/routes/api/player.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const Player = require("../../classes/Player");
+const index = require("../../index");
+
+router.post('/token', (req, res) => {
+  const token = req.body.token;
+  const response = {
+    data: null,
+    error: null
+  };
+  const player = index.checkToken(token);
+  if (player) {
+    response.data = { player }
+  } else {
+    response.error = "No such user."
+  }
+  res.send(response);
+});
+
+router.post('/register', (req, res) => {
+  const { name, password } = req.body;
+  const response = {
+    data: null,
+    error: null
+  };
+  const player = new Player(name, password);
+  index.addNewPlayer(player);
+  response.data = { token: player.getToken };
+  res.send(response);
+});
+
+router.post('/login', (req, res) => {
+  const { name, password } = req.body;
+  const response = {
+    data: null,
+    error: null
+  };
+  const player = index.login(name, password);
+  if (player) {
+    response.data = { token: player.getToken }
+  } else {
+    response.error = "No such user or wrong password."
+  }
+  res.send(response);
+});
+
+router.post('/info', (req, res) => {
+  const token = req.body.token;
+  const response = {
+    data: null,
+    error: null
+  };
+  const player = index.getPlayerInfo(token);
+  if (player) {
+    response.data = { player }
+  } else {
+    response.error = "Something went wrong."
+  }
+  res.send(response);
+});
+
+module.exports = router;
